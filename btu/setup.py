827e12c6cd418abf80caca421504c49aec20a1be
tsotne <tsotnesharvadze@gmail.com>
2022-01-17 15:14:38 +0400

    Changed Version

--- btu/setup.py
+++ btu/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="btu",
-    version="0.0.2",
+    version="0.0.3",
     author="Tsotne Sharvadze",
     author_email="tsotne.sharvadze@btu.edu.ge",
     description="A small example package",
